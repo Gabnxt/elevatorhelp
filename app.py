494fec7e7a7a38c995b00740a1d927f76105e386
Gabnxt <jgolavarria@gmail.com>
2025-07-05 13:26:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -127,4 +127,5 @@ def refresh_cache():
     return "Cache refreshed successfully."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
